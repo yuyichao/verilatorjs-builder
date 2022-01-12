4a3ea8a12197ed24e1404b59d644ba3fe9f2a3e9
Yichao Yu <yyc1992@gmail.com>
2022-01-12 18:26:40 -0500

    Enable LTO

--- scripts/build_verilator.sh
+++ scripts/build_verilator.sh
@@ -3,9 +3,9 @@
 builder_dir=$(realpath "$1")
 source_dir=$(realpath "$2")
 
-export CFLAGS='-g0 -O3'
-export CXXFLAGS='-g0 -O3'
-export LDFLAGS='-O3 -g0 -s INVOKE_RUN=0 -s MODULARIZE=1 -s EXPORTED_RUNTIME_METHODS=["callMain","FS"]'
+export CFLAGS='-g0 -O3 -flto'
+export CXXFLAGS='-g0 -O3 -flto'
+export LDFLAGS='-g0 -O3 -flto -s INVOKE_RUN=0 -s MODULARIZE=1 -s EXPORTED_RUNTIME_METHODS=["callMain","FS"]'
 export EM_NODE_JS="$builder_dir/scripts/node_wrapper"
 
 cd "$source_dir"
